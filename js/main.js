a038d6287bb076044de0747c6b47db96eca33a2c
Agnieszka Tanalska <agnieszka.tanalska@gmail.com>
2018-07-28 15:02:12 +0200

    Fix: Changes suggested by reviewer

--- js/main.js
+++ js/main.js
@@ -141,10 +141,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.setAttribute("alt", `${restaurant.name}'s photo`);
+  image.setAttribute("alt", `${restaurant.name}'s restaurant profile image`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
